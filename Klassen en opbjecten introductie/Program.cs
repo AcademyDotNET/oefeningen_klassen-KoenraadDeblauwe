f930296850e9356fc29a5245e23ea49f38c2c1e8
KoenraadDeblauwe <conrad.deblauwe@gmail.com>
2021-04-13 13:12:12 +0200

    Klassen en objecten oef

---
+++ Klassen en opbjecten introductie/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Klassen_en_opbjecten_introductie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Resultaat mijnBeoordeling = new Resultaat();
+            Console.WriteLine("Geef uw percentage: ");
+            int percentage=int.Parse(Console.ReadLine());
+            mijnBeoordeling.Percentage = percentage;
+            mijnBeoordeling.SetPercentage();
+        }
+    }
+}
---
+++ Klassen en opbjecten introductie/Resultaat.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Klassen_en_opbjecten_introductie
+{
+    class Resultaat
+    {
+        public int Percentage { get; set; }
+        public void SetPercentage()
+        {
+            if (Percentage>0 && Percentage < 50)
+            {
+                Console.WriteLine("Niet geslaagd.");
+
+            }
+            else if (Percentage > 49 && Percentage < 68)
+            {
+                Console.WriteLine("Voldoende.");
+            }
+            else if (Percentage >=68 && Percentage < 75)
+            {
+                Console.WriteLine("Onderscheiding.");
+            }
+            else if (Percentage >75 && Percentage <= 85)
+            {
+                Console.WriteLine("Grote onderscheiding.");
+            }
+            else if (Percentage > 85 && Percentage <= 100)
+            {
+                Console.WriteLine("Grootste onderscheiding.");
+            }
+            else
+            {
+                Console.WriteLine("Verkeerde ingave.");
+            }
+        }
+    }
+}

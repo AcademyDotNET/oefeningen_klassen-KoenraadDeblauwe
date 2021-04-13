c5e705fa5a3f3fc201badbf490beccc888a7c401
KoenraadDeblauwe <conrad.deblauwe@gmail.com>
2021-04-13 13:29:32 +0200

    Studentenklasse oef

---
+++ Studentklasse/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Studentklasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student();
+            student1.Klas = Klassen.EA2;
+            student1.Leeftijd = 21;
+            student1.Naam = "Joske Vermeulen";
+            student1.PuntenCommunicatie = 12;
+            student1.PuntenProgrammingPrinciples = 15;
+            student1.PuntenWebTech = 13;
+            student1.GeefOverzicht();
+
+            Console.ReadLine();
+            Console.Clear();
+
+            Student student2 = new Student();
+            student2.Klas = Klassen.EA3;
+            student2.Leeftijd = 22;
+            student2.Naam = "Koenraad Deblauwe";
+            student2.PuntenCommunicatie = 15;
+            student2.PuntenProgrammingPrinciples = 17;
+            student2.PuntenWebTech = 19;
+
+
+            student2.GeefOverzicht();
+        }
+    }
+}
---
+++ Studentklasse/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Studentklasse
+{
+    public enum Klassen { EA2, EA3 }
+    class Student
+    {
+        
+        
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+        public Klassen Klas { get; set; }
+        public int PuntenCommunicatie { get; set; }
+        public int PuntenProgrammingPrinciples { get; set; }
+        public int PuntenWebTech { get; set; }
+
+        private int myVar;
+
+        public int MyProperty
+        {
+            get { return myVar; }
+            set { myVar = value; }
+        }
+
+  
+
+    
+        public double BerekenTotaalcijfer()
+        {
+            return (((double)PuntenCommunicatie + (double)PuntenProgrammingPrinciples + (double)PuntenWebTech)) / 3;
+        }
+
+        public void GeefOverzicht()
+        {
+            Console.WriteLine($"{Naam}, {Leeftijd} jaar");
+            Console.WriteLine($"Klas: {Klas}\n\n");
+            Console.WriteLine("Cijferrapport:");
+            Console.WriteLine("**************");
+            Console.WriteLine($"Communicatie:\t\t {PuntenCommunicatie}");
+            Console.WriteLine($"Programming Principles:\t{PuntenProgrammingPrinciples}");
+            Console.WriteLine($"Web Technology:\t\t{PuntenWebTech}");
+            Console.WriteLine($"Gemiddelde:\t\t {BerekenTotaalcijfer():F1}");
+        }
+    }
+}
